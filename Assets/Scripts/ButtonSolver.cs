41e190821a2cff29642056e3a8f99d7e76f879a9
Moad Merzouki <moad.merzouki@gmail.com>
2023-04-23 13:17:32 +0200

    Modification bouton pour qu'il s'actualise

---
+++ Assets/Scripts/ButtonSolver.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonSolver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Game game;
+    public Text buttonText;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(game.getSolverRunning())
+        {
+            buttonText.text = "Arrêt du solveur";
+        }
+        else
+        {
+            buttonText.text = "Lancement du solveur";
+        }
+    }
+
+    public void TaskOnClick()
+    {
+        if(game.getSolverRunning())
+        {
+            game.SetSolverRunning(false);
+        }
+        else
+        {
+            game.switchSolver();
+        }
+    } 
+}
