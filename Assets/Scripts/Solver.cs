72e0a3b087a583a0bda47e0e8305591b962cc80e
Nitoute <hamza-chabl@hotmail.com>
2023-04-23 21:44:30 +0200

    retirer bug build

--- Assets/Scripts/Solver.cs
+++ Assets/Scripts/Solver.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 using System.Collections.Generic;
 using System.Threading;
 using System.Diagnostics;
-using static UnityEditor.PlayerSettings;
 using System.IO;
 using System.Threading.Tasks;
 
