6d562879d3c6606a9a58ed1b97d3e0e3a9618c31
vraxXx <vince.ghigo@hotmail.com>
2023-04-23 16:40:58 +0200

    ajout de la position des pions dans la seed manuel

--- Assets/Scripts/ChangeSeedButton.cs
+++ Assets/Scripts/ChangeSeedButton.cs
@@ -13,12 +13,27 @@ public class ChangeSeedButton : MonoBehaviour
     }
     public void TaskOnClick()
     {
+        print("a");
         string[] s = inputfield.getInput().Split(',');
-        int[] seed = new int[4];
+        int[] seed=new int[4];
         for (int i = 0; i< 4; i++)
         {
             seed[i] = int.Parse(s[i]);
         }
+        if (s.Length==12){
+            List<(int,int)> posPion = new List<(int,int)>();
+            for (int i = 4; i< 12; i+=2)
+            {
+                posPion[i] = (int.Parse(s[i]),int.Parse(s[i+1]));
+                try{
+                    game.SetPositionDefaultRobots(posPion);
+                }
+                catch{
+                    print("Les positions données ne sont pas bonnes");
+                }
+            }
+        }
         game.changeSeed(seed[0], seed[1], seed[2], seed[3]);
     }
 }
+
